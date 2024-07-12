289a23e16a7978014be95e19a043f822925fb05e
Amlan Kumar <amlantest23@example.com>
2024-07-12 21:50:02 +0530

    12th July

--- src/test/java/gson/serialization/BookingResponse.java
+++ src/test/java/gson/serialization/BookingResponse.java
@@ -1,2 +1,22 @@
-package gson.serialization;public class BookingResponse {
+package gson.serialization;
+
+public class BookingResponse {
+    private Integer bookingid;
+    private Booking booking;
+
+    public Integer getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(Integer bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
 }
